d27b6d89ec2cdc18fc7fb9f75f9c3c4ef0f81213
Poul-Henning Kamp <phk@FreeBSD.org>
2011-11-04 22:56:47 +0000

    CBM900 symbol work

--- tasks/cbm900/task.py
+++ tasks/cbm900/task.py
@@ -2,6 +2,9 @@
 #
 # This is a demo-task which disassembles the CBM9000 boot EPROM
 #
+# XXX: BKP_Set: label is bogusly rendered
+# XXX: cond_codes wrong, see 0:08b0
+#
 
 #######################################################################
 # Check the python version
@@ -193,16 +196,60 @@ def caseW(a):
 	assert z == 0x1e28
 	z = cpu.disass(y - 2)
 	print("CASE", "l=%d" % l, "y=%x" % y)
+	q = dict()
 	for i in range (0,l):
 		const.w16(p, y + 2 * i)
 		t = y + 2 * l + 4 * i
-		const.w32(p, t)
-		z.flow("cond", "%d" % i, p.m.b32(t))
-		cpu.disass(p.m.b32(t))
+		idx = p.m.b16(y + 2 * i)
+		res = p.m.b32(t)
+		# XXX: should be .PTR
+		x = const.w32(p, t)
+		x.lcmt(" case %04x" % idx)
+		z.flow("cond", "%d" % i, res)
+		cpu.disass(res)
+		q[idx]= res
+	return q
 
-caseW(0x0680)
-caseW(0x31a6)
-caseW(0x3998)
+###############
+# Command-Switch main menu
+q = caseW(0x0680)
+p.setlabel(q[ord('(')], "MENU_BootSpec")
+p.setlabel(q[ord('?')], "MENU_ShowMenu")
+p.setlabel(q[ord('F')], "MENU_Floppy")
+p.setlabel(q[ord('P')], "MENU_ParkDisk")
+p.setlabel(q[ord('S')], "MENU_DiskParam")
+p.setlabel(q[ord('d')], "MENU_Debugger")
+p.setlabel(q[ord('l')], "MENU_LoadBoot")
+p.setlabel(q[ord('m')], "MENU_ShowRam")
+
+###############
+# Command-Switch breakpoints
+q = caseW(0x31a6)
+p.setlabel(q[ord('c')], "BKP_Clear");
+p.setlabel(q[ord('d')], "BKP_Display");
+p.setlabel(q[ord('r')], "BKP_Remove");
+p.setlabel(q[ord('s')], "BKP_Set");
+
+###############
+# Command-Switch debugger
+q = caseW(0x3998)
+p.setlabel(q[ord('?')], "DBG_Display_Help_Menu")
+p.setlabel(q[ord('M')], "DBG_Display_MMU")
+p.setlabel(q[ord('R')], "DBG_Modify_Register")
+p.setlabel(q[ord('S')], "DBG_Remap_MMU")
+p.setlabel(q[ord('a')], "DBG_Set_Base_Adr")
+p.setlabel(q[ord('b')], "DBG_Breakpoints")
+p.setlabel(q[ord('c')], "DBG_ContinueTrap")
+p.setlabel(q[ord('e')], "DBG_EditMem")
+p.setlabel(q[ord('f')], "DBG_FillMem")
+p.setlabel(q[ord('h')], "DBG_HexMath")
+p.setlabel(q[ord('i')], "DBG_Input")
+p.setlabel(q[ord('m')], "DBG_MoveMem")
+p.setlabel(q[ord('o')], "DBG_Output")
+p.setlabel(q[ord('r')], "DBG_Registers")
+p.setlabel(q[ord('s')], "DBG_Display_Stack")
+p.setlabel(q[ord('t')], "DBG_t")
+
 ###############
 for a in range(0x419e, 0x4202, 4):
 	const.w32(p, a)
@@ -256,12 +303,26 @@ p.setlabel(0x0228, "OUTW(adr,data)")
 p.setlabel(0x0234, "JMP(adr)")
 p.setlabel(0x0274, "LDIRB(src,dst,len)")
 
+p.setlabel(0x074c, "DiskParam(char *)")
+p.setlabel(0x07d4, "ShowRam(void)")
+p.setlabel(0x08b0, "int HexDigit(char *)")
+p.setlabel(0x0900, "puts(char *)")
+p.setlabel(0x0998, "ShowMenu(void)")
+p.setlabel(0x09d8, "Floppy(char *)")
+p.setlabel(0x0b26, "puthex(long val,int ndig)")
+p.setlabel(0x0ac0, "ParkDisk(char *)")
+p.setlabel(0x0fc2, "putchar(char)")
+
+p.setlabel(0x20b8, "Debugger(void)")
 p.setlabel(0x20e4, "AvidCHR(RL0,>R10)")
 p.setlabel(0x4224, "BvidCHR(RL0,>R10)")
 p.setlabel(0x21b6, "is_NL")
 p.setlabel(0x2244, "is_CR")
 p.setlabel(0x225e, "is_FF")
 p.setlabel(0x226c, "is_BS")
+p.setlabel(0x231e, "Debugger_Menu()")
+p.setlabel(0x2bb6, "Debugger_MainLoop()")
+
 
 p.setlabel(0x3b28, "OutStr(char*)")
 
@@ -343,6 +404,7 @@ if True:
 	p.g = topology.topology(p)
 	p.g.build_bb()
 	p.g.segment()
+	p.g.setlabels(p)
 	p.g.dump_dot()
 	p.g.xxx(p)
 
