0ae5283776936b011a505fec3f7fec4603c147b2
Poul-Henning Kamp <phk@FreeBSD.org>
2011-11-10 23:43:23 +0000

    Add a MCS650x disassembler, and the CBM900 floppy controller

---
+++ cpus/mcs6500.py
@@ -0,0 +1,162 @@
+#!/usr/local/bin/python
+#
+# MOS Technology 650x disassembler
+#
+
+from __future__ import print_function
+
+import const
+import disass
+
+inscode = (
+#	0	1	2	3	4	5	6	7	8	9	a	b	c	d	e	f
+	"",	"nORA",	"",	"",	"",	"zORA",	"zASL",	"",	"_PHP",	"iORA",	"AASL",	"",	"",	"aORA",	"aASL",	"",
+	"rBPL",	"mORA",	"",	"",	"",	"XORA",	"XASL",	"",	"_CLC",	"yORA",	"",	"",	"",	"xORA",	"xASL",	"",
+	"",	"nAND",	"",	"",	"zBIT",	"zAND",	"zROL",	"",	"_PLP",	"iAND",	"AROL",	"",	"aBIT",	"aAND",	"aROL",	"",
+	"rBMI",	"mAND",	"",	"",	"",	"XAND",	"XROL",	"",	"_SEC",	"",	"",	"",	"",	"xAND",	"xROL",	"",
+
+	"",	"nEOR",	"",	"",	"",	"zEOR",	"zLSR",	"",	"_PHA",	"iEOR",	"ALSR",	"",	"",	"aEOR",	"aLSR",	"",
+	"rBVC",	"mEOR",	"",	"",	"",	"XEOR",	"XLSR",	"",	"_CLI",	"yEOR",	"",	"",	"",	"xEOR",	"xLSR",	"",
+	"",	"nADC",	"",	"",	"",	"zADC",	"zROR",	"",	"_PLA",	"iADC",	"AROR",	"",	"",	"aADC",	"aROR",	"",
+	"rBVS",	"mADC",	"",	"",	"",	"XADC",	"XROR",	"",	"_SEI",	"yADC",	"",	"",	"",	"xADC",	"xROR",	"",
+
+	"",	"nSTA",	"",	"",	"zSTY",	"zSTA",	"zSTX",	"",	"_DEY",	"",	"_TXA",	"",	"aSTY",	"aSTA",	"aSTX",	"",
+	"rBCC",	"mSTA",	"",	"",	"XSTY",	"XSTA",	"XSTY",	"",	"_TYA",	"ySTA",	"_TXS",	"",	"",	"xSTA",	"",	"",
+	"iLDY",	"nLDA",	"iLDX",	"",	"zLDY",	"zLDA",	"zLDX",	"",	"_TAY",	"iLDA",	"_TAX",	"",	"aLDX",	"aLDA",	"aLDX",	"",
+	"rBCS",	"mLDA",	"",	"",	"XLDY",	"XLDA",	"YLDA",	"",	"_CLV",	"yLDA",	"_TSX",	"",	"",	"xLDA",	"yLDX",	"",
+
+	"iCPY",	"nCMP",	"",	"",	"zCPY",	"zCMP",	"zDEC",	"",	"_INY",	"iCMP",	"_DEX",	"",	"aCPY",	"aCMP",	"aDEC",	"",
+	"rBNE",	"mCMP",	"",	"",	"",	"XCMP",	"XDEC",	"",	"_CLD",	"yCMP",	"",	"",	"",	"xCMP",	"xDEC",	"",
+	"iCPX",	"nSBC",	"",	"",	"zCPX",	"zSBC",	"zINC",	"",	"_INX",	"iSBC",	"_NOP",	"",	"aCPX",	"aSBC",	"aINC",	"",
+	"rBEQ",	"mSBC",	"",	"",	"",	"XSBC",	"XINC",	"",	"_SED",	"ySBC",	"",	"",	"",	"xSBC",	"xINC",	"",
+)
+
+class mcs6502(disass.assy):
+	"""
+	"""
+
+	def __init__(self, p, name = "mcs6502"):
+		disass.assy.__init__(self, p, name)
+		assert len(inscode) == 256
+
+	def do_disass(self, adr, ins):
+		assert ins.lo == adr
+		assert ins.status == "prospective"
+
+		p = self.p
+
+		try:
+			iw = p.m.rd(adr)
+		except:
+			print("FETCH failed:", adr)
+			ins.fail("no mem")
+			return
+
+		ic = inscode[iw]
+		#print("%02x " % iw, ic)
+		if ic == "":
+			ic = "-"
+		if ic[0] == "z":
+			# Page Zero address
+			ins.mne = ic[1:]
+			ins.oper.append("%02x" % p.m.rd(adr + 1))
+			ins.hi = ins.lo + 2
+		elif ic[0] == "X":
+			# PZ,X
+			da = p.m.rd(adr + 1)
+			ins.mne = ic[1:]
+			ins.oper.append("%02x" % da)
+			ins.oper.append("X")
+			ins.hi = ins.lo + 2
+		elif ic[0] == "Y":
+			# PZ,Y
+			da = p.m.rd(adr + 1)
+			ins.mne = ic[1:]
+			ins.oper.append("%02x" % da)
+			ins.oper.append("Y")
+			ins.hi = ins.lo + 2
+		elif ic[0] == "i":
+			# Immediate
+			ins.mne = ic[1:]
+			ins.oper.append("#%02x" % p.m.rd(adr + 1))
+			ins.hi = ins.lo + 2
+		elif ic[0] == "a":
+			# Absolute
+			da = p.m.l16(adr + 1)
+			ins.mne = ic[1:]
+			ins.oper.append("%04x" % da)
+			ins.hi = ins.lo + 3
+		elif ic[0] == "x":
+			# Absolute,X
+			da = p.m.l16(adr + 1)
+			ins.mne = ic[1:]
+			ins.oper.append("%04x" % da)
+			ins.oper.append("X")
+			ins.hi = ins.lo + 3
+		elif ic[0] == "y":
+			# Absolute,Y
+			da = p.m.l16(adr + 1)
+			ins.mne = ic[1:]
+			ins.oper.append("%04x" % da)
+			ins.oper.append("Y")
+			ins.hi = ins.lo + 3
+		elif ic[0] == "n":
+			# (Ind,X)
+			da = p.m.rd(adr + 1)
+			ins.mne = ic[1:]
+			ins.oper.append("(%02x,X)" % da)
+			ins.hi = ins.lo + 2
+		elif ic[0] == "m":
+			# (Ind,Y)
+			da = p.m.rd(adr + 1)
+			ins.mne = ic[1:]
+			ins.oper.append("(%02x,Y)" % da)
+			ins.hi = ins.lo + 2
+		elif ic[0] == "r":
+			# Relative
+			ins.mne = ic[1:]
+			da = ins.lo + 2 + p.m.s8(adr + 1)
+			ins.oper.append((da, "%s", "%04x" % da))
+			ins.flow("cond", ic[2:], da)
+			ins.flow("cond", "N" + ic[2:], adr + 2)
+			ins.hi = ins.lo + 2
+		elif ic[0] == "A":
+			# Acc
+			ins.mne = ic[1:]
+			ins.oper.append("A")
+		elif ic[0] == "_":
+			# Implied
+			ins.mne = ic[1:]
+		elif iw == 0x00:
+			ins.mne = "BRK"
+			ins.flow("cond", "IRQ", None)
+		elif iw == 0x20:
+			ins.mne = "JSR"
+			da = p.m.l16(adr + 1)
+			ins.flow("call", "T", da)
+			ins.oper.append((da, "%s", "%04x" % da))
+			ins.hi = ins.lo + 3
+		elif iw == 0x40:
+			ins.mne = "RTI"
+			ins.flow("ret", "IRQ", None)
+		elif iw == 0x6c:
+			# JMP (ind)
+			ins.mne = "JMP"
+			da = p.m.l16(adr + 1)
+			ins.oper.append((da, "(%s)", "(%04x)" % da))
+			ins.flow("cond", "T", None)
+			ins.hi = ins.lo + 3
+		elif iw == 0x4c:
+			# JMP abs
+			ins.mne = "JMP"
+			da = p.m.l16(adr + 1)
+			ins.oper.append((da, "%s", "%04x" % da))
+			ins.flow("cond", "T", da)
+			ins.hi = ins.lo + 3
+		elif iw == 0x60:
+			ins.mne = "RTS"
+			ins.flow("ret", "T", None)
+		else:
+			ins.fail("NYI %02x" % iw)
+
+
---
+++ tasks/cbm900_fd/task.py
@@ -0,0 +1,134 @@
+#!/usr/local/bin/python
+#
+# This is a demo-task which disassembles the CBM9000 boot EPROM
+#
+
+#######################################################################
+# Check the python version
+
+import sys
+assert sys.version_info[0] >= 3 or "Need" == "Python v3"
+
+#######################################################################
+# Set up a search path to two levels below
+
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(".", "..", "..")))
+
+#######################################################################
+# Stuff we need...
+import mem
+import const
+import pyreveng
+import render
+import topology
+import cpus.mcs6500
+
+
+#######################################################################
+# Set up the memory image
+m0 = mem.byte_mem(0, 0x010000, 0, True, "little-endian")
+
+m0.fromfile("EPROM_Z8000_Fl.Cont._S41_6-20-85.bin", 0xe000, 1)
+m0.bcols=8
+
+#######################################################################
+# Create a pyreveng instance
+p = pyreveng.pyreveng(m0)
+
+cpu = cpus.mcs6500.mcs6502(p)
+
+for a in range(0xfffa, 0x10000, 2):
+	const.w16(p, a)
+	cpu.disass(p.m.l16(a))
+for a in range(0xfff3, 0xfff9, 3):
+	cpu.disass(a)
+	pass
+
+for a in range(0xff9a, 0xffa0, 2):
+	const.w16(p, a)
+	#cpu.disass(p.m.l16(a))
+for a in range(0xff93, 0xff9a, 3):
+	const.w16(p, a + 1)
+	#cpu.disass(a)
+	pass
+
+for a in range(0xe291, 0xe29d, 2):
+	da = p.m.l16(a)
+	const.w16(p, a)
+	cpu.disass(da)
+
+cpu.disass(0xe034)
+cpu.disass(0xe906)
+cpu.disass(0xe90c)
+cpu.disass(0xe90e)
+
+cpu.disass(0xe948)
+const.byte(p, 0xe962, 6)
+const.byte(p, 0xe968, 5)
+
+if False:
+	cpu.disass(0xe96d)
+	cpu.disass(0xe972)
+	cpu.disass(0xe977)
+	cpu.disass(0xe97c)
+	cpu.disass(0xe989)
+	cpu.disass(0xe98e)
+	cpu.disass(0xe993)
+	# cpu.disass(0xea33)
+	cpu.disass(0xeab1)
+	cpu.disass(0xeafb)
+	#cpu.disass(0xeb0d)
+	#cpu.disass(0xeb10)
+	cpu.disass(0xeb13)
+	cpu.disass(0xeb40)
+	cpu.disass(0xeb9f)
+	cpu.disass(0xebf1)
+	#cpu.disass(0xebff)
+	cpu.disass(0xec16)
+	cpu.disass(0xec19)
+
+while p.run():
+	pass
+
+cpu.to_tree()
+
+x = p.t.add(0xec27,0xff93, "FILL")
+x.render = "ZFILL"
+x.fold = True
+
+x = p.t.add(0xffa0,0xfff3, "FILL")
+x.render = "ZFILL"
+x.fold = True
+
+#######################################################################
+for a in range(0xe7bf, 0xe7d3, 4):
+	const.byte(p, a, 4)
+
+const.byte(p, 0xe675, 4)
+const.byte(p, 0xe679, 4)
+const.byte(p, 0xe80a, 4)
+const.byte(p, 0xe28a, 4)
+#######################################################################
+
+p.setlabel(0xe048, "ram_err")
+p.setlabel(0xe7d3, "delay()")
+
+#######################################################################
+# Build code graph
+
+if True:
+	p.g = topology.topology(p)
+	p.g.build_bb()
+	p.g.segment()
+	p.g.setlabels(p)
+	p.g.dump_dot()
+	p.g.xxx(p)
+
+#######################################################################
+# Render output
+
+print("Render")
+r = render.render(p)
+r.add_flows()
+r.render("/tmp/_.cbm900_fd.txt")
